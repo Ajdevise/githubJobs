1b83140bc5b2aaf9d24d87a78916c4dc9ccfc7ea
Ajdevise <ajdevise151@hotmail.com>
2021-03-14 09:07:02 +0100

    finished card styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchPageComponent } from './search-page/search-page.component';
+import { JobsListComponent } from './search-page/jobs-list/jobs-list.component';
+import { JobCardComponent } from './search-page/jobs-list/job-card/job-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchPageComponent
+    SearchPageComponent,
+    JobsListComponent,
+    JobCardComponent
   ],
   imports: [
     BrowserModule
